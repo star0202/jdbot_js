c4522e74aaf6ef2a3035c5ff7f2507361c45d21f
Starcea <stardev.uwu@gmail.com>
2022-07-30 17:11:06 +0900

    asdf

--- index.js
+++
@@ -1,9 +0,0 @@
-var _a = require('discord.js'), Client = _a.Client, Intents = _a.Intents;
-require('dotenv').config();
-var token = process.env.TOKEN;
-var client = new Client({ intents: new Intents(32767) });
-client.once('ready', function () {
-    console.log('Ready!');
-    console.log(client.user.username);
-});
-client.login(token);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 const { Client, Intents } = require('discord.js');
 require('dotenv').config();
 const token = process.env.TOKEN;
-const client = new Client({intents: new Intents(32767)});
+const client = new Client({ intents: new Intents(32767) });
 client.once('ready', () => {
 	console.log('Ready!');
-    console.log(client.user.username)
+	console.log(client.user.username);
 });
 client.login(token);
